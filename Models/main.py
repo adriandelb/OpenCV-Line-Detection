56ab292daafcb7891d6667c4e8c8c6bca062c4e4
unknown <adrianmbm8@gmail.com>
2020-06-03 09:31:32 -0500

    added video through webcam

--- Models/main.py
+++ Models/main.py
@@ -1,14 +1,21 @@
-import cv2
+import cv2 as cv
 
-cap = cv2.VideoCapture(0)
+cap  = cv.VideoCapture(0)
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
 print(cap.isOpened())
 
-while (cap.isOpened()):
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
-
-    if cv2.waitKey(1) & 0xff == ord('q'):
+while(cap.isOpened()):
+    ret, frame  = cap.read()
+    if ret == True:
+        out.write(frame)
+        grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        cv.imshow('frame', grey)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
-
 cap.release()
-cv2.destroyAllWindows()
+out.release()
+cv.destroyAllWindows()
