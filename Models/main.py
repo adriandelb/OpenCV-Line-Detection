9334da5a14cdbb02fb7ce8ac6410aa066f4941e1
Adrian Del Bosque <Adrian.DelBosque@spacex.com>
2020-06-26 11:39:11 -0500

    Initial Commit

--- Models/main.py
+++ Models/main.py
@@ -5,15 +5,33 @@ import matplotlib.pyplot as plt
 import time
 
 
+####################################################################################################
+############################### Function Definitions ###############################################
+####################################################################################################
 
+#function for line detection
 def line_detection(cropped,frame):
+    #Local Variables
+    filterSize = (3,3)
+    maxPixelValue = 45
+    minPixelValue = 0
+    kernel = np.ones(filterSize, np.uint8)
+
+    #convert rgb to greyscale
     grey = cv.cvtColor(cropped, cv.COLOR_BGR2GRAY)
-    blur = cv.GaussianBlur(grey,(3,3),0)
-    
-    lineId = cv.inRange(blur, 0, 45)
+
+    #blur image to reduce noise for analysis (The (3,3) is the kernal size, the- 
+    # bigger the filter the more the pixels are combined together)
+    blur = cv.GaussianBlur(grey,filterSize,0)
+
+    #creates a range for acceptable pixel values (This value is changed depending- 
+    #on the intensity of pixel range. [image, lowerbound, Upperbound])
+    lineId = cv.inRange(blur, minPixelValue, maxPixelValue)
    
-    kernel = np.ones((3,3), np.uint8)
+    #erode the line to limit noise near the edges. It brings in the edge. Iteration is- 
+    #the number of times it is run. No more times it runs, the more it is eroded but costs performance
     lineId = cv.erode(lineId,kernel, iterations=1)
+    #expands 
     lineId = cv.dilate(lineId,kernel, iterations=3)
 
     thresh = cv.adaptiveThreshold(lineId, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 17,2)
@@ -24,22 +42,36 @@ def line_detection(cropped,frame):
 
 
 
-
-
 ####################################################################################################
+############################### Main Program #######################################################
+####################################################################################################
+
+
+#path for video file
 path = 'C://users/adelbosque/Documents/Assignments/OpenCV-Line-Detection/videos/video2.mov'
+#opens video file under path
 cap = cv.VideoCapture(path)
+
+
+#loop that runs program with the video is running
 while cap.isOpened():    
+    #reads each frame in cap and stores it in frame
     ret, frame = cap.read()
-    cropped = frame[100:700, 1800:1920]
-   
+
+    #check statement
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
- 
+
+  
+    #crop the original frame size for analysis
+    cropped = frame[100:700, 1800:1920]
+
+    #function call for detecting the line. Function returns an array of contours and draws the contours on the image
     contours_blk, drawn_contours = line_detection(cropped, frame)
-    #print(len(contours_blk))
+    ##############################################################
     
+
     if len(contours_blk) > 0:
         blackbox = cv.minAreaRect(contours_blk[0])
         (x_min, y_min), (w_min, h_min), ang = blackbox
